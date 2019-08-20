cd49f032f61d173a62fae2db06b708a801ba1625
Achede_HD <hernan.david.hd@gmail.com>
2019-08-20 10:51:00 -0300

    /usr/bin/

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@
 # UBUNTU "apt" or RedHat "yum"
 apt install python3 python3-pip unzip wget curl -y
 
+
 ##################### CENTOS 7 no tiene Python 3 oficial ( ni pip )
 yum makecache
 yum install yum-utils
@@ -56,8 +57,8 @@ echo "deberia ser ----> aws-cli/1.16.192 +"
 
 echo "######################### aws-iam-authenticator"
 curl https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/linux/amd64/aws-iam-authenticator -o aws-iam-authenticator
-mv aws-iam-authenticator $HOME/.local/bin/aws-iam-authenticator
-chmod +x $HOME/.local/bin/aws-iam-authenticator
+mv aws-iam-authenticator /usr/bin/aws-iam-authenticator
+chmod +x /usr/bin/aws-iam-authenticator
 
 echo "######################### kubectl"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
@@ -68,41 +69,40 @@ echo "######################### kubens and kubectx"
 curl https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx -o kubectx
 curl https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens -o kubens
 chmod +x kube* 
-mv kube* $HOME/.local/bin/
+mv kube* /usr/bin/
  
 echo "######################### eksctl"
 curl --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-sudo mv /tmp/eksctl /usr/bin
+sudo mv /tmp/eksctl /usr/bin/
 # . <(eksctl completion bash)
 eksctl version
 
 echo "######################### KOPS --> kubectl for clusters"
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
-sudo mv kops-linux-amd64 /usr/local/bin/kops
+sudo mv kops-linux-amd64 /usr/bin/
 
 echo "######################### Terraform"
 curl https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip -o terraform.zip
 unzip terraform.zip
 chmod +x terraform
-mv terraform $HOME/.local/bin/
+mv terraform /usr/bin/
 
 echo "######################### Mozilla SOPS"
 wget https://github.com/mozilla/sops/releases/download/3.2.0/sops-3.2.0.linux
 chmod +x sops-3.2.0.linux
-mv sops $HOME/.local/bin/sops
+mv sops /usr/bin/
 
 echo "######################### stern"
 wget https://github.com/wercker/stern/releases/download/1.10.0/stern_linux_amd64
 chmod +x stern_linux_amd64
-mv stern_linux_amd64 $HOME/.local/bin/stern
+mv stern_linux_amd64 /usr/bin/
 
 echo "######################### helm"
 wget https://storage.googleapis.com/kubernetes-helm/helm-v2.9.1-linux-amd64.tar.gz
 tar -xvf helm-v2.9.1-linux-amd64.tar.gz
 chmod +x ./linux-amd64/helm
-
-mv ./linux-amd64/helm $HOME/.local/bin/helm
+mv ./linux-amd64/helm /usr/bin/
 
 
 # aws configure  # AWS access keys
