b98a66b6bdcd31e76d626e725f48196b30b91db0
Achede_HD <hernan.david.hd@gmail.com>
2019-09-17 16:35:18 -0300

    /usr/local/bin/

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@
 # UBUNTU "apt" or RedHat "yum"
 apt install python3 python3-pip unzip wget curl -y
 
-
 ##################### CENTOS 7 no tiene Python 3 oficial ( ni pip )
 yum makecache
 yum update -y
@@ -74,7 +73,7 @@ echo "######################### kubens and kubectx"
 curl https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx -o kubectx
 curl https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens -o kubens
 chmod +x kube* 
-mv kube* /usr/bin/
+mv kube* /usr/local/bin/
  
 echo "######################### eksctl"
 curl --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
@@ -85,29 +84,29 @@ eksctl version
 echo "######################### KOPS --> kubectl for clusters"
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
-sudo mv kops-linux-amd64 /usr/bin/
+sudo mv kops-linux-amd64 /usr/local/bin/
 
 echo "######################### Terraform"
 curl https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip -o terraform.zip
 unzip terraform.zip
 chmod +x terraform
-mv terraform /usr/bin/
+mv terraform /usr/local/bin/
 
 echo "######################### Mozilla SOPS"
 wget https://github.com/mozilla/sops/releases/download/3.2.0/sops-3.2.0.linux
 chmod +x sops-3.2.0.linux
-mv sops /usr/bin/
+mv sops /usr/local/bin/
 
 echo "######################### stern"
 wget https://github.com/wercker/stern/releases/download/1.10.0/stern_linux_amd64
 chmod +x stern_linux_amd64
-mv stern_linux_amd64 /usr/bin/
+mv stern_linux_amd64 /usr/local/bin/
 
 echo "######################### helm"
 wget https://storage.googleapis.com/kubernetes-helm/helm-v2.9.1-linux-amd64.tar.gz
 tar -xvf helm-v2.9.1-linux-amd64.tar.gz
 chmod +x ./linux-amd64/helm
-mv ./linux-amd64/helm /usr/bin/
+mv ./linux-amd64/helm /usr/local/bin/
 
 
 ## aws configure  # AWS access keys
@@ -119,5 +118,19 @@ mv ./linux-amd64/helm /usr/bin/
 # aws eks --region us-east-1 update-kubeconfig --name regulada-develop_eks
 
 ## Added new context arn:aws:eks:us-east-1:947293917990:cluster/regulada-develop_eks to /root/.kube/config
+
+##############
+
+cat ~/.aws/credentials
+#[regulado-develop]
+#aws_access_key_id = XXXXXXXXXXXXXXXXXX
+#aws_secret_access_key = XXXXXXXXXXXXXX
+
+#[sandbox-devops]
+#aws_access_key_id = XXXXXXXXXXXXXX
+#aws_secret_access_key = XXXXXXXXXXXXXX
+
+# export AWS_PROFILE=sandbox-devops
+
 
 
